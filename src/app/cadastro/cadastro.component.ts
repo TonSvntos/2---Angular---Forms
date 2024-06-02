d47b264207429ec5a7d4b13cf7adece1c8678a07
Ton DeVeros <TonDeveros@gmail.com>
2024-06-02 13:05:20 -0300

    first commit

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,52 @@
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ConsultaCepService } from "../service/consulta-cep.service";
+
+@Component({
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styleUrls: ["./cadastro.component.css"],
+})
+export class CadastroComponent implements OnInit {
+  constructor(
+    private router: Router, 
+    private cepService: ConsultaCepService
+  ) {}
+
+  ngOnInit(): void {}
+
+  cadastrar(form: NgForm) {
+    console.log(form.controls);
+    if (form.valid) {
+      // this.router.navigate(['./sucesso']);
+      // return
+    } else {
+      alert("formulario invalido");
+    }
+  }
+
+  consultaCEP(event: any,f: NgForm) {
+    const cep = event.target.value;
+
+
+    if(cep !== ''){
+      this.cepService
+      .getConsultaCep(cep)
+      .subscribe((resultado) => this.populandoEndereco(resultado, f));
+    }
+  }
+
+
+
+  populandoEndereco(dados: any, f: NgForm){
+    f.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf
+    })
+  }
+
+}
